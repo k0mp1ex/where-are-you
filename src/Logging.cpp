cb44b724a8f314644592b3db03e28540a39e8159
k0mp1ex <112667618+k0mp1ex@users.noreply.github.com>
2023-01-02 23:04:08 -0300

    feat: add debug support for logging

--- src/Logging.cpp
+++ src/Logging.cpp
@@ -1,18 +1,24 @@
 #include <spdlog/sinks/basic_file_sink.h>
+#include <spdlog/sinks/msvc_sink.h>
 #include "Logging.h"
 
 namespace kxWhereAreYou::Logging {
     void Setup() {
         auto logsFolder = SKSE::log::log_directory();
-        if (!logsFolder) {
-            SKSE::stl::report_and_fail("SKSE log_directory not provided, logs disabled.");
-        }
+        if (!logsFolder) SKSE::stl::report_and_fail("SKSE log_directory not provided, logs disabled.");
         auto pluginName = SKSE::PluginDeclaration::GetSingleton()->GetName();
         auto logFilePath = *logsFolder / std::format("{}.log", pluginName);
         auto fileLoggerPtr = std::make_shared<spdlog::sinks::basic_file_sink_mt>(logFilePath.string(), true);
-        auto loggerPtr = std::make_shared<spdlog::logger>("log", std::move(fileLoggerPtr));
+        std::shared_ptr<spdlog::logger> loggerPtr;
+        if (IsDebuggerPresent()) {
+            auto debugLoggerPtr = std::make_shared<spdlog::sinks::msvc_sink_mt>();
+            spdlog::sinks_init_list loggers{std::move(fileLoggerPtr), std::move(debugLoggerPtr)};
+            loggerPtr = std::make_shared<spdlog::logger>("log", loggers);
+        } else {
+            loggerPtr = std::make_shared<spdlog::logger>("log", std::move(fileLoggerPtr));
+        }
         spdlog::set_default_logger(std::move(loggerPtr));
         spdlog::set_level(spdlog::level::trace);
-        spdlog::flush_on(spdlog::level::info);
+        spdlog::flush_on(spdlog::level::trace);
     }
 }
